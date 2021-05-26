b1dbca598727e494c7d3f68dec0c26d33d9acb91
pejuangbitco <irsyadeinstein@gmail.com>
2021-05-26 12:51:26 +0700

    add filter jenis barang terjual terbanyak

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,12 +6,34 @@ class Home extends BaseController
 {
 	function __construct()
 	{
+		helper('url');
+    $this->api = 'http://localhost:5000/api/inventory/';
 		$this->client = \Config\Services::curlrequest();
 	}
 
 	public function index()
 	{
-		return view('welcome_message');
+		$response = $this->client->get($this->api);
+		return view('layout', [
+			'title' => 'Data Inventory',
+      'page'  => 'content_inventory.php',
+			'data'	=> json_decode($response->getBody(), true)
+		]);
+	}
+
+	public function filter()
+	{		
+		$data = $this->request->getGet();
+		$this->api .= 'filter';
+		if ($data)
+			$this->api .= '?&from='.$data['from'].'&to='.$data['to'];
+			
+		$response = $this->client->get($this->api);
+		return view('layout', [
+			'title' => 'Data Inventory',
+      'page'  => 'content_inventory_filter.php',
+			'data'	=> json_decode($response->getBody(), true)
+		]);
 	}
 
 	public function test()
---
+++ app/Views/content_inventory.php
@@ -0,0 +1,60 @@
+<section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+
+        <div class="card">
+          <div class="card-header">
+          Bandingkan jenis barang Dari: <form method="get" action="<?= base_url('Home/filter') ?>"><input class="form-control" type="date" name="from"> Sampai: <input class="form-control" type="date" name="to"> <button type="submit" class="btn btn-primary">Submit</button></form>
+          </div>
+          <!-- /.card-header -->
+          <div class="card-body">            
+            <table id="example1" class="table table-bordered table-striped">
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Barang</th>
+                  <th>Stok</th>
+                  <th>Jumlah Terjual</th>
+                  <th>Tanggal Transaksi</th>
+                  <th>Jenis Barang</th>
+                  <!-- <th>Aksi</th> -->
+                </tr>
+              </thead>
+              <tbody>
+                <?php 
+                $i = 1;
+                  foreach($data as $d) {
+                    echo '<tr>
+                      <td>'.$i.'</td>
+                      <td>'.$d['name'].'</td>
+                      <td>'.$d['stock'].'</td>
+                      <td>'.$d['sold'].'</td>
+                      <td>'.$d['tanggal'].'</td>
+                      <td>'.$d['productType'].'</td>';
+                      
+                    $i++;
+                  }
+                ?>
+              </tbody>
+              <!-- <tfoot>
+                <tr>
+                  <th>No</th>
+                  <th>Nama Barang</th>
+                  <th>Stok</th>
+                  <th>Jenis Barang</th>
+                  <th>Aksi</th>
+                </tr>
+              </tfoot> -->
+            </table>
+          </div>
+          <!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+      </div>
+      <!-- /.col -->
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container-fluid -->
+</section>
